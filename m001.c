60ced04f290b6c21d15f8a3152ac79c1766cae90
Rafael Vieira de Santana <62967728+Rafael-Vieira-de-Santana@users.noreply.github.com>
2020-06-18 10:54:18 -0300

    Update m001.c

--- m001.c
+++ m001.c
@@ -24,14 +24,14 @@
 
 int greatest (int x, int y, int z)
 { 
-	if (x >= y && x >= z) {           /* If the first variable is greater or equal to the other two,                                                         */
-	return x; }                       /* return it.                                                                                                          */
+	if (x >= y && x >= z)            /* If the first variable is greater or equal to the other two,                                                         */
+	return x;                        /* return it.                                                                                                          */
   
-	else if (y >= x && y >= z){       /* If the condidion above isn't met, test if the second variable is greater or equal to the other two.                 */
-	return y; }                       /* if it is, return it.                                                                                                */
+	else if (y >= x && y >= z)       /* If the condidion above isn't met, test if the second variable is greater or equal to the other two.                 */
+	return y;                        /* if it is, return it.                                                                                                */
   
-	else {                            /* if neither of the condidions is met, the only possibility is that the third variable is greather than the other two.*/
-	return z; }                       /* so, if that is true, return the final variable.                                                                     */
+	else                             /* if neither of the condidions is met, the only possibility is that the third variable is greather than the other two.*/
+	return z;                        /* so, if that is true, return the final variable.                                                                     */
 }
 
 /* Do not edit this function. */
