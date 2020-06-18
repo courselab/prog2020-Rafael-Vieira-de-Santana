895cb3b20e8466dfccfd241b7c9203cb2c712949
Rafael Vieira de Santana <62967728+Rafael-Vieira-de-Santana@users.noreply.github.com>
2020-06-18 10:20:39 -0300

    Update m001.c

--- m001.c
+++ m001.c
@@ -22,21 +22,16 @@
 
 /* Return the greatest among a, b and c. */
 
-int greatest (int a, int b, int c)
+int greatest (int x, int y, int z)
 {
-	printf  ("enter 3 values: ");
-	scanf   ("%d %d %d", &a, &b, &c);
-	
-	if      (a >= b && a >= c){     /* If the first variable is greater or equal to the other two,                                                         */
-}             /* return it.                                                                                                          */
+	if (x >= y && x >= z)        /* If the first variable is greater or equal to the other two,                                                         */
+	return x;                    /* return it.                                                                                                          */
   
-    else if (b >= a &&  b >= c) {   /* If the condidion above isn't met, test if the second variable is greater or equal to the other two.                 */
-    a = b   }          /* if it is, return it.                                                                                                */
+	else if (y >= x &&  y >= z)      /* If the condidion above isn't met, test if the second variable is greater or equal to the other two.                 */
+	return y;                        /* if it is, return it.                                                                                                */
   
-    else       {                    /* if neither of the condidions is met, the only possibility is that the third variable is greather than the other two.*/
-    a = c  }           /* so, if that is true, return the final variable.                                                                     */
-    
-    return a;
+	else                             /* if neither of the condidions is met, the only possibility is that the third variable is greather than the other two.*/
+	return z;                        /* so, if that is true, return the final variable.                                                                     */
 }
 
 /* Do not edit this function. */
