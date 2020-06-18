2b9c6e6238ff474e1019ff5665e678449b18bb3d
Rafael Vieira de Santana <62967728+Rafael-Vieira-de-Santana@users.noreply.github.com>
2020-06-18 10:26:16 -0300

    Update m001.c

--- m001.c
+++ m001.c
@@ -27,7 +27,7 @@ int greatest (int x, int y, int z)
 	if (x >= y && x >= z) {           /* If the first variable is greater or equal to the other two,                                                         */
 	return x; }                       /* return it.                                                                                                          */
   
-	else if (y >= x &&  y >= z) {     /* If the condidion above isn't met, test if the second variable is greater or equal to the other two.                 */
+	else if (y >= x && y >= z){       /* If the condidion above isn't met, test if the second variable is greater or equal to the other two.                 */
 	return y; }                       /* if it is, return it.                                                                                                */
   
 	else {                            /* if neither of the condidions is met, the only possibility is that the third variable is greather than the other two.*/
