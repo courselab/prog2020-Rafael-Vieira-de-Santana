44b1750f4dd06363c90838e7bf7b200fc022ec3b
Rafael Vieira de Santana <62967728+Rafael-Vieira-de-Santana@users.noreply.github.com>
2020-06-20 15:55:42 -0300

    Update m008.c

--- m008.c
+++ m008.c
@@ -102,7 +102,7 @@ void lastname (char *s)
 	for (i = 0; a[i] != 0; ++i) //copies all the contents from the vector "a" to the vector "s".
 	s[i] = a[i];
 	
-	if (state = 1)
+	if (state == 1)
 	{
 		s[i] = ' ';
 		s[i +1] = 0;
@@ -115,7 +115,7 @@ void lastname (char *s)
 	for testing purposes:
 	printf("current name: %s\n\n", a);
 	printf("numberofspaces = %d, firstletter = %d, firstspace = %d, count = %d\n\n", numberofspaces, firstletter, firstspace, count);
-	/*
+	*/
 }
 
 
